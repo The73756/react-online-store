c1de6a142aeeb4d3b5918f1ccc7c3ab4192ae5e3
The73756 <the73756the@gmail.com>
2022-09-25 14:57:45 +0900

    add checking user role

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const brandController = require('../controllers/brandController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
-router.post('/', brandController.create);
+router.post('/', checkRole('ADMIN'), brandController.create);
 router.get('/', brandController.getAll);
-router.delete('/', brandController.delete);
+router.delete('/', checkRole('ADMIN'), brandController.delete);
 
 module.exports = router;
--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const deviceController = require('../controllers/deviceController');
+const checkRole = require('../middleware/checkRoleMiddleware');
 
-router.post('/', deviceController.create);
+router.post('/', checkRole('ADMIN'), deviceController.create);
 router.get('/', deviceController.getAll);
-router.get('/:id', deviceController.getOne);
+router.get('/:id', deviceController.getOne); //сделать delete
 
 module.exports = router;
