d2ffd7fbf254301ba41af60a9231264a9f1372e3
The73756 <the73756the@gmail.com>
2022-09-25 11:33:53 +0900

    start routes + endpoints

---
+++ server/controllers/userController.js
@@ -0,0 +1,12 @@
+class UserController {
+  async registration(req, res) {}
+
+  async login(req, res) {}
+
+  async check(req, res) {
+    const query = req.query;
+    res.json(query);
+  }
+}
+
+module.exports = new UserController();
