2c07feb3aade02b6fc64ce482502115606f15e59
The73756 <the73756the@gmail.com>
2022-10-08 20:07:37 +0900

    start client-server interaction

---
+++ client/src/http/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const $host = axios.create({
+	baseURL: process.env.REACT_APP_API_URL,
+});
+
+const $authHost = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const authInstance = config => {
+	config.headers.authorization = `Bearer ${localStorage.getItem('token')}`;
+	return config;
+}
+
+$authHost.interceptors.request.use(authInstance);
+
+export {
+	$host,
+	$authHost
+}
