6bfc68775188ddbc73ff085657feb703e75f7a44
The73756 <the73756the@gmail.com>
2022-10-11 19:13:41 +0900

    complete login/registration + check jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
+import { observer } from 'mobx-react-lite';
+import { useContext, useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import AppRouter from './components/AppRouter';
-
+import { Context } from '.';
 import './App.scss';
+import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
-const App = () => {
+import { check } from './http/userApi';
+
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [isLoading, setIsLoading] = useState(true); // TODO: прикрутить какой то лоадер
+
+  useEffect(() => {
+    check()
+      .then((data) => {
+        user.setUser(true);
+        user.setIsAuth(true);
+      })
+      .catch(() => {
+        user.setUser(false);
+        user.setIsAuth(false);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
       <AppRouter />
     </BrowserRouter>
   );
-};
+});
 
 export default App;
--- client/src/components/AuthComponent/index.jsx
+++ client/src/components/AuthComponent/index.jsx
@@ -1,18 +1,61 @@
-import { Link, useLocation } from 'react-router-dom';
-import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../../utils/consts';
+import { observer } from 'mobx-react-lite';
+import { useContext, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Context } from '../..';
+import { login, registration } from '../../http/userApi';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from '../../utils/consts';
+
 import styles from './AuthComponent.module.scss';
 
-const AuthComponent = () => {
+const AuthComponent = observer(() => {
+  const { user } = useContext(Context);
   const location = useLocation().pathname;
+  const navigate = useNavigate();
   const isLogin = location !== REGISTRATION_ROUTE;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data;
+
+    try {
+      if (isLogin) {
+        data = await login(email, password);
+      } else {
+        data = await registration(email, password);
+      }
+      user.setUser(user);
+      user.setIsAuth(true);
+      navigate(SHOP_ROUTE);
+    } catch (error) {
+      alert(error.response.data.message);
+      console.log(error);
+    }
+
+    console.log(data);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <h2 className={styles.title}>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
-        <form action='' className={styles.form}>
-          <input type='text' className={styles.formItem} placeholder='Введите ваш e-mail' />
-          <input type='text' className={styles.formItem} placeholder='Введите ваш пароль' />
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.formItem}
+            placeholder='Введите ваш e-mail'
+          />
+          <input
+            type='password'
+            autoComplete='on'
+            className={styles.formItem}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Введите ваш пароль'
+          />
           <div className={styles.formBottom}>
             {isLogin ? (
               <span>
@@ -30,12 +73,14 @@ const AuthComponent = () => {
               </span>
             )}
 
-            <button className={styles.formButton}>{isLogin ? 'Войти' : 'Регистрация'}</button>
+            <button type='submit' className={styles.formButton}>
+              {isLogin ? 'Войти' : 'Регистрация'}
+            </button>
           </div>
         </form>
       </div>
     </div>
   );
-};
+});
 
 export default AuthComponent;
